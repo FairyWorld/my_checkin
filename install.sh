3f0ec29a5f770c657292e666491c2eef1537c020
clannad <clannad@qq.com>
2022-09-21 16:29:54 +0800

    fixed

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@
 # new Env('my_checkin安装&&依赖安装');
 if [ ! -f "/ql/data/config/FairyWorld_my_checkin_main/config.yml" ]; then
  echo "生成配置文件 路径ql/data/config/FairyWorld_my_checkin_main/config.yml"
- cp /ql/data/repo/FairyWorld_my_checkin_main/config.yml.temple /ql/data/config/FairyWorld_my_checkin_main/config.yml
+ cp -r /ql/data/repo/FairyWorld_my_checkin_main/config.yml.temple /ql/data/config/FairyWorld_my_checkin_main/config.yml
 else
  echo "已存在配置文件"
  echo "生成空白模板 /ql/data/config/FairyWorld_my_checkin_main/config.yml.sample"
- cp /ql/data/repo/FairyWorld_my_checkin_main/config.yml.temple /ql/data/config/FairyWorld_my_checkin_main/config.yml.sample
+ cp -r /ql/data/repo/FairyWorld_my_checkin_main/config.yml.temple /ql/data/config/FairyWorld_my_checkin_main/config.yml.sample
 fi
 
 echo "cookie填写请去config/FairyWorld_my_checkin_main/config.yml填写 青龙面板也有文件编辑功能"
